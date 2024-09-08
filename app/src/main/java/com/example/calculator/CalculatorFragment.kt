3bbede607732a93ecb90a8e2a67eff44ec37df6c
Yuki Naka <yukinaka2168@au.com>
2024-09-08 10:32:26 +0900

    「build.gradle.kts(:app)」に「viewBinding = true」を追加。
    「CalculatorFragment」内でViewBinding。

--- app/src/main/java/com/example/calculator/CalculatorFragment.kt
+++ app/src/main/java/com/example/calculator/CalculatorFragment.kt
@@ -5,18 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.calculator.databinding.FragmentCalculatorBinding
 
 
 class CalculatorFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentCalculatorBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_calculator, container, false)
+    ): View {
+        _binding = FragmentCalculatorBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        binding.setOnClickListener {
+//        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
